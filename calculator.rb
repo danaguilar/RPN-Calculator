01ece9971795f6e200686ed8e77c1cdd71066e02
Dan Aguilar <danaguilar6@gmail.com>
2017-08-27 22:52:00 -0500

    Added Calculator IO
    Added the IO for the calculator. Kept it in the same file for simplicity but would probably have split it into multiple files in a larger project for easier testing.

--- calculator.rb
+++ calculator.rb
@@ -9,14 +9,18 @@ class Calculator
 		end
 	end
 
+	# List of all current allowed operations for this calculator. Can be updated by simply adding a new operation to the array
+
 	OPERATIONS = [
 		Operation.new('Addition','+',2, lambda { |nums| return nums[0] + nums[1]}),
-		Operation.new('Substraction','-',2, lambda { |nums| return nums[0] - nums[1]}),
+		Operation.new('Subtraction','-',2, lambda { |nums| return nums[0] - nums[1]}),
 		Operation.new('Multiplication', '*',2, lambda { |nums| return nums[0] * nums[1]}),
 		Operation.new('Division','/',2, lambda { |nums|
-			raise ZeroDivisionError, "Zero Division Error: Cannot divide #{nums[0]} by #{nums[1]}" if nums[1] == 0
+			raise ZeroDivisionError, "ZERO DIVISION ERROR: Cannot divide #{nums[0]} by #{nums[1]}" if nums[1] == 0
 			return nums[0] / nums[1]
 			})
+		# To use another operation, add the new operation to this array with the following syntax
+		# Operation.new('OperationName','OperationChar',number_of_operands, lambda { ** operation logic **})
 	]
 
 	attr_reader :stack
@@ -27,8 +31,9 @@ class Calculator
 		@saved_stack = []
 	end
 
-	def new_command(input)
-		if compute(input)
+	# The only public method for this object. Takes in a new expression from an outside source and either computes it or rejects it with an error message
+	def new_expression(input)
+		if compute_expression(input)
 			return [true, latest_result]
 		else
 			return [false, error_message]
@@ -37,10 +42,11 @@ class Calculator
 
 	private
 
-		def compute(str)
+		# Backs up the current stack and attempts to run the given expression. If an error is found, catches the error, records it, and reverts the stack back to its old state
+		def compute_expression(str)
 			save_state
 			begin
-				parse_command_string(str)
+				parse_expression_string(str)
 			rescue ArgumentError, ZeroDivisionError => e
 				record_error(e.message)
 				revert_state
@@ -49,23 +55,26 @@ class Calculator
 			return true
 		end
 
-		def parse_command_string(str)
+		# Splits the expression into individual commands and processes them in order
+		def parse_expression_string(str)
 			command_array = str.split(' ')
 			command_array.each do |command|
 				process_command(command)
 			end
 		end
 
+		# Processes an individual command. If it's a number, it stores the number. If it's a valid operation, it attempts to run the operation. Otherwise it throws and error
 		def process_command(command)
 			if is_number?(command)
 				add_number_to_stack(command)
 			elsif is_operation(command)
 				run_operation(command)
 			else
-				raise ArgumentError, "ArgumentError: #{command} is neither a number nor a recognized operation"
+				raise ArgumentError, "ARGUMENT ERROR: #{command} is neither a number nor a recognized operation"
 			end
 		end
 
+		# Takes a valid operation character and attempts to run that operation. Throws an error if there are not enough operands for the given operation. Adds the result to the stack if it is successful
 		def run_operation(command)
 			operation = get_operation(command)
 			number_of_operands = operation.argument_size
@@ -73,7 +82,7 @@ class Calculator
 				operands = get_operands(number_of_operands)
 				result = operation.function.call(operands)
 			else
-				raise ArgumentError, "Argument Error: #{operation.name} requires #{operation.argument_size} operands. Only #{@stack.length} given."
+				raise ArgumentError, "ARGUMENT ERROR: #{operation.name} requires #{operation.argument_size} operands. Only #{@stack.length} given."
 			end
 			add_number_to_stack(result)
 		end
@@ -89,37 +98,68 @@ class Calculator
 			return @stack[-1]
 		end
 
+		# Records and error message for future use
 		def record_error(msg)
 			@error = msg
 		end
 
+		# Returns the most recent recorded error message
 		def error_message
 			return @error
 		end
 
+		# Saves the current stack in a backup
 		def save_state
 			@saved_stack = @stack.dup
 		end
 
+		# Returns the stack to its last saved state
 		def revert_state
 			@stack = @saved_stack
 		end
 
+		# Returns and removes the given number of operands from the stack.
 		def get_operands(number_of_operands)
 			return @stack.pop(number_of_operands)
 		end
 
+		# Returns the Operation object stored in the OPERATIONS constant that is related to the given character
 		def get_operation(char)
 			operation_index = OPERATIONS.find_index{ |operation| char == operation.character}
 			return OPERATIONS[operation_index]
 		end
 
+		# Regex expression meant to test if a given string is a number
 		def is_number?(test_str)
-			test_str =~ /\A[-+]?[0-9]*\.?[0-9]+\Z/
+			test_str =~ /\A[-]?[0-9]*\.?[0-9]+\Z/
 		end
 
+		# Returns true if the given character corresponds to a Operation object in the OPERATIONS constant
 		def is_operation(char)
 			return true if OPERATIONS.find_index{ |operation| char == operation.character}
 			return false
 		end
 end
+
+
+class CalculatorIO
+	def initialize
+		@calculator = Calculator.new
+	end
+
+	def run
+		while true
+			print "input expression > "
+			expression = gets.chomp
+			break if expression == 'q' || expression == '' #   is the ASCII end-of-transmission command character most commonly seen in the terminal with ctrl-D
+			result = @calculator.new_expression(expression)
+			result[0] ? puts("result: #{result[1].to_f.round(3)}") : puts("#{result[1]}")
+		end
+		puts "Quitting..."
+		puts "Thanks for using the RPN calculator"
+	end
+end
+
+
+calc = CalculatorIO.new
+calc.run if ARGV.empty? # Only run if no arguments are given. Therefore this run will be skipped during rspec's test runs
--- calculator_spec.rb
+++ calculator_spec.rb
@@ -13,49 +13,49 @@ describe Calculator  do
 	describe 'Input' do
 		context 'When given a number...' do
 			it 'accept a single number' do
-				expect(@calculator.new_command('3')[0]).to be true
+				expect(@calculator.new_expression('3')[0]).to be true
 			end
 			it 'accept multiple numbers' do
-				expect(@calculator.new_command('3 4.23 5 78 168')[0]).to be true
+				expect(@calculator.new_expression('3 4.23 5 78 168')[0]).to be true
 			end
 		end
 		context 'When given a character...' do
 			context 'for characters that are valid operations...' do
 				context "and there are enough given operands" do
 					it 'accept addition(+)' do
-						expect(@calculator.new_command('5 2 +')[0]).to be true
+						expect(@calculator.new_expression('5 2 +')[0]).to be true
 					end
 					it 'accept subtraction(-)' do
-						expect(@calculator.new_command('5 2 -')[0]).to be true
+						expect(@calculator.new_expression('5 2 -')[0]).to be true
 					end
 					it 'accept multiplication(*)' do
-						expect(@calculator.new_command('5 2 *')[0]).to be true
+						expect(@calculator.new_expression('5 2 *')[0]).to be true
 					end
 					it 'accept division(/)' do
-						expect(@calculator.new_command('5 2 /')[0]).to be true
+						expect(@calculator.new_expression('5 2 /')[0]).to be true
 					end
 					it 'reject division over 0' do
-						expect(@calculator.new_command('5 0 /')[0]).to be false
+						expect(@calculator.new_expression('5 0 /')[0]).to be false
 					end
 				end
 				context "and there are not enough given operands..."do
 					it 'reject addition(+)' do
-						expect(@calculator.new_command('2 +')[0]).to be false
+						expect(@calculator.new_expression('2 +')[0]).to be false
 					end
 					it 'reject subtraction(-)' do
-						expect(@calculator.new_command('-')[0]).to be false
+						expect(@calculator.new_expression('-')[0]).to be false
 					end
 					it 'reject multiplication(*)' do
-						expect(@calculator.new_command('5 *')[0]).to be false
+						expect(@calculator.new_expression('5 *')[0]).to be false
 					end
 					it 'reject division(/)' do
-						expect(@calculator.new_command('5 /')[0]).to be false
+						expect(@calculator.new_expression('5 /')[0]).to be false
 					end
 				end
 			end
 		context 'for characters that are not valid operations...' do
 				it 'reject non valid operations' do
-					expect(@calculator.new_command('2 5 4 %')[0]).to be false
+					expect(@calculator.new_expression('2 5 4 %')[0]).to be false
 				end
 			end
 		end
@@ -65,48 +65,48 @@ describe Calculator  do
 		context 'When given a single line equation...' do
 			context 'if there is one operation...' do
 				it 'perform addition correctly' do
-					expect(@calculator.new_command('5 2 +')[1]).to eq(7)
+					expect(@calculator.new_expression('5 2 +')[1]).to eq(7)
 				end
 				it 'perform subtraction correctly' do
-					expect(@calculator.new_command('5 2 -')[1]).to eq(3)
+					expect(@calculator.new_expression('5 2 -')[1]).to eq(3)
 				end
 				it 'perform multiplication correctly' do
-					expect(@calculator.new_command('5 2 *')[1]).to eq(10)
+					expect(@calculator.new_expression('5 2 *')[1]).to eq(10)
 				end
 				it 'perform division correctly' do
-					expect(@calculator.new_command('10 2 /')[1]).to eq(5)
+					expect(@calculator.new_expression('10 2 /')[1]).to eq(5)
 				end
 			end
 			context 'For multiple operations...' do
 				it 'perform operations from left to right' do
-					expect(@calculator.new_command('15 7 1 1 + - / 3 * 2 1 1 + + -')[1]).to eq(5)
+					expect(@calculator.new_expression('15 7 1 1 + - / 3 * 2 1 1 + + -')[1]).to eq(5)
 				end
 			end
 		end
 		context 'When given a multi line equation...' do
 			it 'save given numbers for future use' do
-				@calculator.new_command('3')
-				@calculator.new_command('4')
-				@calculator.new_command('5')
+				@calculator.new_expression('3')
+				@calculator.new_expression('4')
+				@calculator.new_expression('5')
 				expect(@calculator.stack).to eq([3.0, 4.0, 5.0])
 			end
 			it 'use saved numbers for operations' do
-				@calculator.new_command('3')
-				@calculator.new_command('4')
-				@calculator.new_command('5')
-				expect(@calculator.new_command('+')[1]).to eq(9)
-				expect(@calculator.new_command('*')[1]).to eq(27)
+				@calculator.new_expression('3')
+				@calculator.new_expression('4')
+				@calculator.new_expression('5')
+				expect(@calculator.new_expression('+')[1]).to eq(9)
+				expect(@calculator.new_expression('*')[1]).to eq(27)
 			end
-			context 'If an error occurs during a new_command...' do
+			context 'If an error occurs during a new_expression...' do
 				it 'return error message' do
-					@calculator.new_command('3')
-					@calculator.new_command('4')
-					@calculator.new_command('5')
-					expect(@calculator.new_command('ERROR')[1]).to match /Error/
+					@calculator.new_expression('3')
+					@calculator.new_expression('4')
+					@calculator.new_expression('5')
+					expect(@calculator.new_expression('ERROR')[1]).to match /ERROR/
 				end
-				it 'revert to the state before new_command was given' do
-					@calculator.new_command('3')
-					@calculator.new_command('0 /')
+				it 'revert to the state before new_expression was given' do
+					@calculator.new_expression('3')
+					@calculator.new_expression('0 /')
 					expect(@calculator.stack).to eq([3.0])
 				end
 			end
